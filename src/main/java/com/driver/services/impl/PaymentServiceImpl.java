f4c31bd4c49fc40041cf79987401d092c3357744
Deepesh Gupta <guptad28031998@gmail.com>
2023-09-23 21:24:06 +0530

    try5

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -3,12 +3,14 @@ package com.driver.services.impl;
 import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
 import com.driver.model.Reservation;
+import com.driver.model.Spot;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.util.Optional;
 
 @Service
@@ -45,6 +47,9 @@ public class PaymentServiceImpl implements PaymentService {
         payment.setPaymentCompleted(true);
         payment.setPaymentMode(paymentMode);
         Payment savedPayment = paymentRepository2.save(payment);
+        Spot spot = reservation.getSpot();
+        spot.setOccupied(false);
+        reservation.setSpot(spot);
         reservationRepository2.save(reservation);
         return  savedPayment;
     }
